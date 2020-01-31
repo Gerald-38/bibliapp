9a95b00c444761a0efce4635441ace760feaa0ed
Gérald <gerald.navarette@gmail.com>
2020-01-31 11:56:02 +0100

    Commit 1 : commit initial version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibliapp';
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyDqlTM_zXt2TkXYfqd9VT2A786t-YS1fzs',
+      authDomain: 'bibliapp-backend.firebaseapp.com',
+      databaseURL: 'https://bibliapp-backend.firebaseio.com',
+      projectId: 'bibliapp-backend',
+      storageBucket: 'bibliapp-backend.appspot.com',
+      messagingSenderId: '1016353018496',
+      appId: '1:1016353018496:web:2c5f04417f684ea1318c65'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+
 }
